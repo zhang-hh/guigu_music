57fd7c2761dc8a42c8e8d102885c4ff4ce2ab5f0
zhang-hh <94918628@qq.com>
2020-03-27 23:25:37 +0800

    小程序(未分包版)

--- pages/recommendSong/recommendSong.js
+++ pages/recommendSong/recommendSong.js
@@ -1,6 +1,6 @@
 // pages/recommendSong/recommendSong.js
 import request from "../../utils/request";
-
+import Pubsub from 'pubsub-js';
 Page({
 
   /**
@@ -10,11 +10,18 @@ Page({
     day:'',
     month:'',
     recommendList:[], //每日推荐歌曲
+    songIndex:0,//当前音乐的下标
+
   },
   /*跳转到歌曲界面*/
   toSongs(event){
-    //点击的时候获取音乐的id
+    console.log(event.currentTarget.dataset)
+    //点击的时候获取音乐的id 和 音乐的下标
     let musicId = event.currentTarget.dataset.songid;
+    let songIndex = event.currentTarget.dataset.songindex;
+    this.setData({
+      songIndex
+    });
     wx.navigateTo({
       /*支持query传参,但是路由传递参数一定是字符串,
        如果传递的不是会自动的toString方法
@@ -48,6 +55,26 @@ Page({
     let recommendListData =  await request('/recommend/songs');
     this.setData({
       recommendList:recommendListData.recommend
+    })
+
+    //消息订阅
+    Pubsub.subscribe('switchMusic',(msg,type) =>{
+        var {songIndex,recommendList} = this.data;
+        if (type === 'pre'){ //上一首的逻辑
+          //预处理上边界
+          (songIndex === 0) && (songIndex =recommendList.length)
+          songIndex -= 1
+        }else { //下一首的逻辑
+          //预处理下边界
+          (songIndex === recommendList.length-1) && (songIndex= -1)
+          songIndex += 1
+        }
+        let musicId = recommendList[songIndex].id;
+        //将musicId发送给song  消息发布
+        Pubsub.publish('musicId',musicId);
+        this.setData({
+          songIndex
+        });
     })
   },
 
--- pages/song/song.js
+++ pages/song/song.js
@@ -1,5 +1,7 @@
 // pages/song/song.js
 import request from "../../utils/request";
+import Pubsub from 'pubsub-js';
+import moment from "moment";
 let appInstance = getApp();
 Page({
   /**
@@ -8,7 +10,10 @@ Page({
   data: {
     isPlay:false,//标识着页面是否播放
     song:{}, //歌曲信息
-    musicId:0
+    musicId:0,
+    momentDurationTime:'00:00',//当前音乐的时长
+    momentCurrentTime:'00:00',//当前音乐的播放时长
+    currentWidth:0,//当前进度条的宽度
   },
   //控制音乐播放暂停
   playControl(){
@@ -17,7 +22,6 @@ Page({
   },
 
   async musicControl(isPlay,musicId){
-    this.backgroundAudioManager =  wx.getBackgroundAudioManager();
     if (isPlay){ //播放的逻辑
       let songData = await request('/song/url',{id:musicId})
       let musicUrl = songData.data[0].url;
@@ -28,12 +32,50 @@ Page({
       appInstance.globalData.globalMusicId =musicId;
       appInstance.globalData.globalIsPlay = true;
 
+      //声明歌曲正在播放 不能切换
+      this.isSwitch = false;
+
     }else { //暂停的逻辑
       this.backgroundAudioManager.pause();
       appInstance.globalData.globalIsPlay = false;
     }
     this.setData({
       isPlay
+    })
+  },
+
+  //切换歌曲的事件回调
+  switchMusic(event){
+    //判断当前音乐是否正常播放
+    if (this.isSwitch) return // 当前的音乐正在切换
+    this.isSwitch = true; /*函数节流*/
+    let type = event.currentTarget.dataset.type;
+    //关闭上一首音乐的播放
+    this.setData({isPlay:false});
+    //停止音乐的播放
+    this.backgroundAudioManager.stop();
+    this.handleSwitchMusic(type)
+  },
+
+  /*封装切换歌曲的方法*/
+  async handleSwitchMusic(type){
+  //  两个页面之间如何通信
+    //1.将切换的类型(上一首或下一首)的信号要发送给recommendSong
+    Pubsub.publish('switchMusic',type);
+    Pubsub.subscribe('musicId',async (msg,musicId) =>{
+      //获取音乐的详细信息
+      let songData = await request('/song/detail',{ids:musicId});
+
+      this.setData({
+        song: songData.songs[0],
+        musicId
+      });
+      this.musicControl(true,musicId);
+      wx.setNavigationBarTitle({
+        title: this.data.song.name
+      });
+      //取消当前的订购
+      Pubsub.unsubscribe('musicId')
     })
   },
 
@@ -49,19 +91,51 @@ Page({
         isPlay:true
       })
     }
+    this.backgroundAudioManager =  wx.getBackgroundAudioManager();
+    /*监听音乐播放进度*/
+    this.backgroundAudioManager.onTimeUpdate(() =>{
+      let momentCurrentTime = moment(this.backgroundAudioManager.currentTime * 1000).format('mm:ss');
+      let currentWidth = this.backgroundAudioManager.currentTime / this.backgroundAudioManager.duration * 410;
+      this.setData({
+        momentCurrentTime,
+        currentWidth
+      })
+    });
+    //监听音乐的播放/暂停/停止的逻辑
+    /*音乐暂停时*/
+    this.backgroundAudioManager.onPause(() =>{
+      this.setData({
+        isPlay:false
+      })
+    });
+    /*音乐停止时*/
+    this.backgroundAudioManager.onStop(() =>{
+      this.setData({
+        isPlay:false
+      })
+    });
+    /*音乐播放时*/
+    this.backgroundAudioManager.onPlay(() => {
+      this.setData({
+        isPlay:true
+      })
+    });
     //console.log(typeof musicId) String
-    //获取音乐的详细信息
+    /*获取音乐的详细信息*/
     let songData = await request('/song/detail',{ids:musicId});
+    //处理音乐时长
+    let durationTime = songData.songs[0].dt; //毫秒
+    let momentDurationTime = moment(durationTime).format('mm:ss')
     this.setData({
       song: songData.songs[0],
-      musicId
+      musicId,
+      momentDurationTime,
     });
     //更新窗口的标题
     wx.setNavigationBarTitle({
       title: this.data.song.name
     });
   },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
